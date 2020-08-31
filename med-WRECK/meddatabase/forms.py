24748cd95f66867a8fb4e35517af0973492c2805
askmydog <stephen.koplin@gmail.com>
2020-08-31 07:46:44 -0400

    Updated Models

--- med-WRECK/meddatabase/forms.py
+++ med-WRECK/meddatabase/forms.py
@@ -5,6 +5,6 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _   
  
 class MedRecForm(forms.Form):
-    query = forms.CharField(widget=forms.Textarea, help_text="paste in meds from AHLTA")
+    query = forms.TextField(widget=forms.Textarea, label='Input Meds Here', help_text="Paste or free-text meds here")
     
     
--- med-WRECK/meddatabase/models.py
+++ med-WRECK/meddatabase/models.py
@@ -6,7 +6,8 @@ from datetime import date
 class Indication(models.Model):
     """Model representing a medication indication"""
     name = models.CharField(max_length=200, help_text='Enter a medication indication (ex: hypertension)')
-    
+    plainlang = models.CharField(max_length=200, help_text='Enter a patient-friendly descriptor for the indication (e.g. "blood pressure" for hypertension)')
+
     class Meta:
         ordering = ['name']
     
@@ -35,19 +36,33 @@ class Route(models.Model):
 		
 	def __str__(self):
 		return self.abbr_route
-	
+    
+class MedQualifiers(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Medication Qualifiers", help_text="Enter a commonly recognized abbreviation for this qualifier (e.g. XR for Extended Release)")
+    terms = models.TextField(max_length=500, verbose_name="Similar Qualifier Terms", help_text="Enter any other terms that relate to this qualifier, separated by commas (for XR, also enter DR, SR, CR, etc)")
+
+	class Meta:
+		ordering = ['med_qualifier']
+		
+	def __str__(self):
+		return self.med_qualifier
+
 class Med(models.Model):
     """Model representing a medication but not a specific patient's medication'"""
-    medication_name = models.CharField(max_length=200)
+    
+    generic_name = models.CharField(max_length=200)
+    match_1st = models.CharField(max_length=200, blank=True null=True)
+    match_2nd = models.CharField(max_length=200, blank=True null=True)
+    qualifiers = models.ManyToManyField('MedQualifiers', blank=True, null=True, help_text="Add any terms that will help distinguish this medication from other similar medications such as forumalation or strength")
     phonetic_name = models.CharField(max_length=200, help_text='Spell the medication name phonetically')
-    brand_name = models.CharField(max_length=200, help_text='Brand name of the medication')
+    trade_name = models.CharField(max_length=200, help_text='Brand name of the medication')
     med_class = models.ForeignKey('Med_Class', verbose_name='Medication Class', help_text='Enter the name of the medication class', on_delete=models.SET_NULL, null=True)
     # Foreign Key used because book can only have one author, but authors can have multiple books
     # Author as a string rather than object because it hasn't been declared yet in the file
     dose = models.CharField('Dose', max_length = 10)
-    route = models.ForeignKey(Route, on_delete=models.SET_NULL, null=True)    
+    route = models.ForeignKey(Route, on_delete=models.SET_NULL, null=True) 
     mechanism_of_action = models.TextField(max_length=1000, help_text='Enter a brief description of the mechanism of action')
-  
+    notes = models.TextField(max_length=500, help_text='Enter any additional notes')
     
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
     # Genre class has already been defined so we can specify the object above.
@@ -124,3 +139,7 @@ class Patient(models.Model):
 class Med_Class(models.Model):
     class_name = models.CharField(max_length=100, verbose_name='Medication Class', help_text='Enter a medication class')
     interactions = models.ManyToManyField('Med_Class', verbose_name='Interactions', help_text='Enter the classes this medication interacts with',blank=True, null=True)
+
+    def get_absolute_url(self):
+        """Returns the url to access a detail record for this med"""
+        return reverse('med-class', args=[str(self.id)])
